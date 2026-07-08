4170a053ca283a08a7de1890544cc3037c3c95c7
HuiYoujie <42954611+HuiYoujie@users.noreply.github.com>
2026-07-08 14:48:00 +0800

    Add Taro app config

---
+++ src/app.config.js
@@ -0,0 +1,32 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+    'pages/fanlist/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#F8F8F8',
+    navigationBarTitleText: 'mahjong',
+    navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#7A7E83',
+    selectedColor: '#3b82f6',
+    borderStyle: 'black',
+    backgroundColor: '#ffffff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'static/index.png',
+        selectedIconPath: 'static/index-active.png'
+      },
+      {
+        pagePath: 'pages/fanlist/index',
+        text: '番表',
+        iconPath: 'static/fanlist.png',
+        selectedIconPath: 'static/fanlist-active.png'
+      }
+    ]
+  }
+});

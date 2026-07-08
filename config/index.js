746d7c36aa51eaceb3ee4d00791b418cb03be309
HuiYoujie <42954611+HuiYoujie@users.noreply.github.com>
2026-07-08 14:47:38 +0800

    Add Taro build config

---
+++ config/index.js
@@ -0,0 +1,35 @@
+const config = {
+  projectName: 'mahjong-calculator',
+  date: '2026-07-08',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [
+    '@tarojs/plugin-framework-react',
+    '@tarojs/plugin-platform-weapp',
+    '@tarojs/plugin-platform-h5'
+  ],
+  defineConstants: {},
+  copy: {
+    patterns: [
+      { from: 'static', to: 'dist/static' }
+    ],
+    options: {}
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  mini: {},
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static'
+  }
+};
+
+module.exports = function () {
+  return config;
+};
